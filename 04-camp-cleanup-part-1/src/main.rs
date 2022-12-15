8b0f4e1a197bd487dd2493ed6c9516424b7930ef
Akaanksh Raj <akaankshraj@gmail.com>
2022-12-15 02:22:50 -0600

    Finish day 4 part 1 and 2

---
+++ 04-camp-cleanup-part-1/src/main.rs
@@ -0,0 +1,25 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        match fs::read_to_string(&args[1]) {
+            Ok(input) => {
+                let mut count: i32 = 0;
+                for line in input.lines() {
+                    let sections: Vec<&str> = line.split(",").collect();
+                    let first_elf: Vec<i32> = sections[0].split("-").map(|s| s.parse().unwrap()).collect();
+                    let second_elf: Vec<i32> = sections[1].split("-").map(|s| s.parse().unwrap()).collect();
+
+                    if (first_elf[0] <= second_elf[0] && second_elf[1] <= first_elf[1]) || (second_elf[0] <= first_elf[0] && first_elf[1] <= second_elf[1]) {
+                        count += 1;
+                    }
+                }
+                println!("Fully contained count: {}", count)
+            }
+            Err(_) => println!("Could not read from provided file."),
+        }
+    } else {
+        println!("Must provide input.txt file as an argument.")
+    }
+}
