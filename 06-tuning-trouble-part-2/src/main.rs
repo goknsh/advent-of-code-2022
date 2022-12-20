b8267a5edb88015aa773b3b897783b6b4227de25
Akaanksh Raj <akaankshraj@gmail.com>
2022-12-19 21:00:50 -0600

    Finish day 6

---
+++ 06-tuning-trouble-part-2/src/main.rs
@@ -0,0 +1,34 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        match fs::read_to_string(&args[1]) {
+            Ok(input) => {
+                let mut position: i32 = 0;
+                let mut last_distinct: Vec<char> = Vec::with_capacity(13);
+                for line in input.lines() {
+                    let letters: Vec<char> = line.chars().collect();
+
+                    for letter in letters {
+                        position += 1;
+                        if last_distinct.len() == 13 && !last_distinct.contains(&letter) {
+                            break;
+                        } else if last_distinct.contains(&letter) {
+                            while last_distinct.get(0) != Some(&letter) {
+                                last_distinct.remove(0);
+                            }
+                            last_distinct.remove(0);
+                        }
+                        last_distinct.push(letter);
+
+                    }
+                }
+                println!("Start-of-packet marker detected: {}", position)
+            }
+            Err(_) => println!("Could not read from provided file."),
+        }
+    } else {
+        println!("Must provide input.txt file as an argument.")
+    }
+}
